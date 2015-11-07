13c3e38406b38a07b7f20571aee82ead03280082
eberman99@gmail.com <ethanberman@Ethans-MacBook-Pro.local>
2015-11-07 15:53:18 -0500

    added to enemy class

--- CodeDay_Project/src/Enemy.java
+++ CodeDay_Project/src/Enemy.java
@@ -1,3 +1,5 @@
+import java.awt.Image;
+
 
 public class Enemy {
 	//stats of the enemy
@@ -5,5 +7,12 @@ public class Enemy {
 	public double attack;
 	public double style;
 	public String label;
-	
+	public Image image;
+	public Enemy(double health, double attack, double style,String label, Image image){
+		this.health = health;
+		this.attack = attack;
+		this.style = style;
+		this.label = label;
+		this.image = image;
+	}
 }
