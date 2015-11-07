afbba8cb872e48402ca49babf2c75c4d2c9569e3
eberman99@gmail.com <ethanberman@Ethans-MacBook-Pro.local>
2015-11-07 16:04:04 -0500

    Update with taunt and fight

--- CodeDay_Project/src/Enemy.java
+++ CodeDay_Project/src/Enemy.java
@@ -8,11 +8,35 @@ public class Enemy {
 	public double style;
 	public String label;
 	public Image image;
+	public boolean lost;
 	public Enemy(double health, double attack, double style,String label, Image image){
 		this.health = health;
 		this.attack = attack;
 		this.style = style;
 		this.label = label;
 		this.image = image;
+	}
+	public void fight(Hero h, Enemy e){
+		double hattack = h.attack*Math.random();
+		double eattack = e.attack*Math.random();
+		if(hattack > eattack){
+			e.health/=2;
+		}else if(hattack<eattack){
+			h.health/=2;
+		}else if(hattack == eattack){
+			h.style+= 10;
+			e.style+= 10;
+		}
+	}
+	//taunt method if one outstyles the other then the other runs away.
+	public void taunt(Hero h, Enemy e){
+		double hstyle = h.style * Math.random();
+		double estyle = e.style * Math.random();
+		if(hstyle > e.style){
+			e.lost = true;
+		}
+		else{
+			e.attack += 5;
+		}
 	}
 }
