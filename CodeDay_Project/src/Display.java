c059d23f12ef7f122c06339cec587796853eac8c
LucasNVarella <lnvarella@gmail.com>
2015-11-07 17:41:16 -0500

    Display

--- CodeDay_Project/src/Display.java
+++ CodeDay_Project/src/Display.java
@@ -1,4 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Display {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("Game");
+		frame.setLocation(0,0);
+		frame.setSize(1080,720);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+	}
 
+
+	public void output(String s) {
+		
+		
+		
+	}
+	
+	public void move(String move) {    // player move
+		
+		
+		
+	}
+	
+	public void updateStats(String move) {
+		
+		
+		
+	}
+	
+	public void updateMap(String move) {   // new towns
+		
+		
+		
+	}
+	
+	public void newGun(String s) {
+		
+		
+		
+	}
+	
+	public void newHat(String s) {
+		
+		
+		
+	}
+	
 }
