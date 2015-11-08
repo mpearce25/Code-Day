47766575a72efd7c5fcde3457f53cb53c5382291
eberman99@gmail.com <ethanberman@Ethans-MacBook-Pro.local>
2015-11-07 19:53:06 -0500

    final enemy hideout

--- CodeDay_Project/src/Enemy.java
+++ CodeDay_Project/src/Enemy.java
@@ -3,7 +3,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 
-public class Enemy implements KeyListener{
+public class Enemy{
 	//stats of the enemy
 	public double health;
 	public double attack;
@@ -16,72 +16,5 @@ public class Enemy implements KeyListener{
 		this.attack = attack;
 		this.style = style;
 		this.label = label;
-	}
-	
-	Hero h = new Hero();
-	public void fight(Hero h, Enemy e){
-		while(e.health > 0){
-		double hattack = h.attack*Math.random();
-		double eattack = e.attack*Math.random();
-		if(hattack > eattack){
-			e.health-= hattack;
-		}else if(hattack<eattack){
-			h.health-= eattack;
-		}else if(hattack == eattack){
-			h.style+= 10;
-			e.style+= 10;
-		}
-		if(e.health == 0){
-			Display.output("You have killed the bandit!");
-		}
-		}
-	}
-	//method to run away
-	public void run(Hero h, Enemy e){
-		if(Math.random() >= .75){
-			lost = true;
-		}
-	}
-	//taunt method if one outstyles the other then the other runs away.
-	public void taunt(Hero h, Enemy e){
-		double hstyle = h.style * Math.random();
-		double estyle = e.style * Math.random();
-		if(hstyle > e.style){
-			e.lost = true;
-			display.output("They ran away! You are too cool for them!");
-			}
-		else{
-			e.attack += 5;
-		}
-	}
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		switch(e.getKeyCode()){
-		//f - Fight
-		case 70:
-			fight(h, this);
-			break;
-		//t - Taunt
-		case 84:
-			taunt(h, this);
-			break;
-		//r - Run away
-		case 82:
-			run(h, this);
-			break;
-		}
-		
-		
-	}
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 }
