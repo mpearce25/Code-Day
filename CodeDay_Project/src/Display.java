114af7844d2191ae917aede8fde1a8b0d6a80e75
mpearce25 <pearcematthew25@gmail.com>
2015-11-07 19:35:36 -0500

    display working

--- CodeDay_Project/src/Display.java
+++ CodeDay_Project/src/Display.java
@@ -3,55 +3,103 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Display {
+	
+	private static JFrame frame;
+	private static JPanel stats;
+	private static JPanel map;
+	private static JScrollPane scrollPane;
+	private static JTextArea console;
+	private static JToolBar toolBar;
+	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
+		
+		frame = new JFrame();
+		// intiating the frame
 		frame.setTitle("Game");
+		frame.setLayout(null);
 		frame.setLocation(0,0);
-		frame.setSize(1080,720);
+		frame.setSize(1080,750);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setUndecorated(true);
 		frame.setVisible(true);
 		
-		JPanel map = new JPanel();
-		JPanel stats = new JPanel();
-		JScrollPane scrollPane = new JScrollPane();
-		JTextArea console = new JTextArea;
-		scrollPane.add();
+		///map
+		map = new JPanel();
+		map.setLayout(null);
+		frame.add(map);
+		
+		map.setLocation(10, 185);
+		map.setSize(700, 525);
+		map.setBorder(BorderFactory.createLineBorder(Color.black));
+
+		//stats
+		stats = new JPanel();
+		frame.add(stats);
+		stats.setLayout(null);
+		
+		stats.setLocation(720, 10);
+		stats.setSize(350,600);
+		stats.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		
+		//console
+		scrollPane = new JScrollPane();
+		frame.add(scrollPane);
+		scrollPane.setLayout(null);
+		
+		console = new JTextArea();
+		scrollPane.add(console);
+		
+		scrollPane.setSize(700, 165);
+		scrollPane.setLocation(10, 10);
+		
+		
+		//toolBar
+		toolBar = new JToolBar();
+		frame.add(toolBar);
+		
+		toolBar.setLocation(720,620);
+		toolBar.setSize(350, 90);
+		toolBar.setFloatable(false);
+		toolBar.setFocusable(false);
+		toolBar.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		//toolbar buttons
+		
 		
 	}
 
 
-	public void output(String s) {
+	public static void output(String s) {
 		
 		
 		
 	}
 	
-	public void move(String move) {    // player move
+	public static void move(String move) {    // player move
 		
 		
 		
 	}
 	
-	public void updateStats(String move) {
+	public static void updateStats(String move) {
 		
 		
 		
 	}
 	
-	public void updateMap(String move) {   // new towns
+	public static void updateMap(String move) {   // new towns
 		
 		
 		
 	}
 	
-	public void newGun(String s) {
+	public static void newGun(String s) {
 		
 		
 		
 	}
 	
-	public void newHat(String s) {
+	public static void newHat(String s) {
 		
 		
 		
